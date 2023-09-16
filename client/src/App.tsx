efa515a77e512e11496f1d0653dfc434fc171384
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-16 14:33:37 +0300

    Import components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
+import Hero from "./components/Hero/Hero";
+import Navbar from "./components/Navbar/Navbar";
+import PopularProducts from "./components/PopularProducts/PopularProducts";
+import CameraInfo from "./components/CameraInfo/CameraInfo";
+import CustomerService from "./components/CustomService/CustomerService";
+import CustomerFeedback from "./components/CustomerFeedback/CustomerFeedback";
+import Subscribe from "./components/Subscribe/Subscribe";
+import Footer from "./components/Footer/Footer";
+
 function App() {
-
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <main className="relative">
+            <Navbar />
+            <section className="xl:padding-l wide:padding-r padding-b">
+                <Hero />
+            </section>
+            <section className="padding">
+                <PopularProducts />
+            </section>
+            <section className="padding">
+                <CameraInfo />
+            </section>
+            <section className="padding">
+                <CustomerService />
+            </section>
+            <section className="padding bg-slate-gray">
+                <CustomerFeedback />
+            </section>
+            <section className="padding padding-y padding-x">
+                <Subscribe />
+            </section>
+            <section className="padding bg-black">
+                <Footer />
+            </section>
+        </main>
+    );
 }
 
-export default App
+export default App;
