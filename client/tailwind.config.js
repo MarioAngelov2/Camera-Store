210f53c17a80fab3cb9ed89c38cec8f69dbbe03b
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-16 13:49:12 +0300

    Config tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '58px'],
+      '6xl': ['68px', '78px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#ECEEFF",
+        "coral-red": "#FF6452",
+        "slate-gray": "#F8F8F9",
+        "blue": "#6A90B6",
+        "white-400": "rgba(255, 255, 255, 0.80)"
+      },
+      boxShadow: {
+        '2xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+    },
   },
   plugins: [],
 }
