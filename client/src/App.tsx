d2f420fb8c609e18cb50ff27f9ec11f92856dce7
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-30 12:38:52 +0300

    Add routes & separate home page from routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import PopularProducts from "./components/PopularProducts/PopularProducts";
@@ -6,34 +7,51 @@ import CustomerService from "./components/CustomService/CustomerService";
 import CustomerFeedback from "./components/CustomerFeedback/CustomerFeedback";
 import Subscribe from "./components/Subscribe/Subscribe";
 import Footer from "./components/Footer/Footer";
+import Catalog from "./components/Catalog/Catalog";
+// import Layout from "./Layout";
 
 function App() {
     return (
         <main className="relative">
             <Navbar />
-            <section className="xl:padding-l wide:padding-r padding-b">
-                <Hero />
-            </section>
-            <section className="padding">
-                <PopularProducts />
-            </section>
-            <section className="padding">
-                <CameraInfo />
-            </section>
-            <section className="padding">
-                <CustomerService />
-            </section>
-            <section className="padding bg-slate-gray">
-                <CustomerFeedback />
-            </section>
-            <section className="padding padding-y padding-x">
-                <Subscribe />
-            </section>
-            <section className="padding bg-black">
-                <Footer />
-            </section>
+            <Routes>
+                <Route path="/" element={<GlobalRoute />} />
+                <Route path="/catalog" element={<Catalog />} />
+            </Routes>
         </main>
     );
+
+    function GlobalRoute() {
+        const shouldShowGlobalContent = true;
+
+        if (shouldShowGlobalContent) {
+            return (
+                <>
+                    <section className="xl:padding-l wide:padding-r padding-b">
+                        <Hero />
+                    </section>
+                    <section className="padding">
+                        <PopularProducts />
+                    </section>
+                    <section className="padding">
+                        <CameraInfo />
+                    </section>
+                    <section className="padding">
+                        <CustomerService />
+                    </section>
+                    <section className="padding bg-slate-gray">
+                        <CustomerFeedback />
+                    </section>
+                    <section className="padding padding-y padding-x">
+                        <Subscribe />
+                    </section>
+                    <section className="padding bg-black">
+                        <Footer />
+                    </section>
+                </>
+            );
+        }
+    }
 }
 
 export default App;
