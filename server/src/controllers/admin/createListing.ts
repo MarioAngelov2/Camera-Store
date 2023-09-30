b60065f1079441f821b9c2583385bb413ddbb7e8
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-30 21:52:54 +0300

    Create controller for listing products

---
+++ server/src/controllers/admin/createListing.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import { CameraModel } from "../../models/camera";
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+
+const createListing = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const token = req.cookies.token;
+
+        const { name, price, discountPrice, cameraType, description, photos } =
+            req.body;
+
+        if (!name || !price || !cameraType) {
+            throw new createHttpError.BadRequest("Missing required fields");
+        }
+
+        const decodedToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET as string
+        );
+
+        if (!decodedToken) {
+            throw new createHttpError.Unauthorized("Unauthorized");
+        }
+
+        const newCamera = new CameraModel({
+            name,
+            price,
+            discountPrice,
+            cameraType,
+            description,
+            photos,
+        });
+
+        await newCamera.save();
+        res.json(newCamera);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default createListing;
