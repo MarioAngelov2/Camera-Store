d762e8f320687b24ef324385bde46cefd8ad443f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-30 09:50:00 +0300

    Add route for login

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import register from '../controllers/users/register'
+import loginUser from "../controllers/users/login";
 
 const router = Router();
 
 router.post('/register', register);
+router.get('/login', loginUser);
 
 export default router;
