6a4c6f487c3b652fb756b016e910cd079a8d327d
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-09-30 21:53:21 +0300

    Update router.ts

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import register from '../controllers/users/register'
 import loginUser from "../controllers/users/login";
+import createListing from "../controllers/admin/createListing";
 import auth from '../middleware/auth'
+import adminAuth from "../middleware/adminAuth";
 
 const router = Router();
 
 router.post('/register', register);
 router.get('/login', loginUser);
+
+router.post('/admin/create-listing', adminAuth, createListing);
 
 export default router;
