ddf9e677bd111cb9bdb23aff8c9d64ff9bff125f
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-01 10:45:43 +0300

    Update router.ts

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
-import register from '../controllers/users/register'
+import register from "../controllers/users/register";
 import loginUser from "../controllers/users/login";
 import createListing from "../controllers/admin/createListing";
 import deleteListing from "../controllers/admin/deleteListing";
-import auth from '../middleware/auth'
+import getListings from "../controllers/admin/getListings";
+import auth from "../middleware/auth";
 import adminAuth from "../middleware/adminAuth";
 
 const router = Router();
 
-router.post('/register', register);
-router.get('/login', loginUser);
+router.post("/register", register);
+router.get("/login", loginUser);
 
-router.post('/admin/create-listing',  createListing);
-router.put('/admin/update-listing/:id', createListing);
-router.delete('/admin/delete-listing/:id', deleteListing)
+router.post("/admin/create-listing", createListing);
+router.put("/admin/update-listing/:id", createListing);
+router.delete("/admin/delete-listing/:id", deleteListing);
+router.get("/admin/get-listings", getListings);
 
 export default router;
