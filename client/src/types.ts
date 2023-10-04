26880d2db5b0e41f745fa40bc8cf3b14f918ef7e
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-04 16:32:37 +0300

    Create types.ts

---
+++ client/src/types.ts
@@ -0,0 +1,5 @@
+type FormEv = React.FormEvent<HTMLFormElement>;
+type MouseEv = React.MouseEvent<HTMLButtonElement>;
+type ChangeEv = React.ChangeEvent<HTMLInputElement>;
+
+export type { FormEv, MouseEv, ChangeEv };
