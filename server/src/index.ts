88f648fc60c253a6c3890f40ff036138f1a1b291
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-04 16:32:55 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173'
+}));
 
 mongoose
     .connect(process.env.MONGO_CONNECTION_STRING)
