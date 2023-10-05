c0bf7be4043d64aef968470669565f658033d760
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-05 15:13:01 +0300

    Minor cors config & add static middleware for images

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,15 +4,21 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import mongoose, { mongo } from "mongoose";
 import router from "./routes/router";
+import path from "path";
+
+const PATH_TO_UPLOADS = path.join(__dirname, "../assets/uploads");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials: true,
-    origin: 'http://127.0.0.1:5173'
-}));
+app.use("/uploads", express.static(PATH_TO_UPLOADS));
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://127.0.0.1:5173",
+    })
+);
 
 mongoose
     .connect(process.env.MONGO_CONNECTION_STRING)
