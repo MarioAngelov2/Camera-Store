2d3a094e930110eec1b8c28efea8829cb806c7f5
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-05 15:12:26 +0300

    Update router.ts

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -7,17 +7,37 @@ import getListings from "../controllers/admin/getListings";
 import auth from "../middleware/auth";
 import adminAuth from "../middleware/adminAuth";
 import logout from "../controllers/users/logout";
+import uploadByLink from "../controllers/admin/uploadByLink";
+import uploadFromDevice from "../controllers/admin/uploadFromDevice";
+import multer from "multer";
+import path from "path";
+
+const parentDir = path.resolve(__dirname, "../../");
+const PATH_TO_UPLOADS = path.join(parentDir, "/assets/uploads/");
+const photosMiddleware = multer({ dest: PATH_TO_UPLOADS });
 
 const router = Router();
 
+// general routes
 router.post("/register", register);
 router.get("/login", loginUser);
 
+
+// upload photos
+router.post("/admin/upload-by-link", uploadByLink);
+router.post(
+    "/admin/upload",
+    photosMiddleware.array("photos", 5),
+    uploadFromDevice
+);
+
+// admin routes
 router.post("/admin/create-listing", createListing);
 router.put("/admin/update-listing/:id", createListing);
 router.delete("/admin/delete-listing/:id", deleteListing);
 router.get("/admin/get-listings", getListings);
 router.post("/admin/login", loginUser);
-router.get("/admin/logout", logout)
+router.get("/admin/logout", logout);
+
 
 export default router;
