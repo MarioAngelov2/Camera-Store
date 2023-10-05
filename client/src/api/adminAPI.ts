0e801b64dbcc10d05560ac2349455e0707c052d0
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-10-05 15:13:56 +0300

    Minor refactor & add api for uploading images

--- client/src/api/adminAPI.ts
+++ client/src/api/adminAPI.ts
@@ -1,35 +1,55 @@
 const adminURL = "http://localhost:3001/admin";
 
-export async function loginAdmin(data: any) {
+type LoginData = {
+    email: string;
+    password: string;
+};
+
+type CreateListing = {
+    name: string;
+    price: string;
+    discountPrice: string;
+    cameraType: string;
+    description: string;
+    photos: string[];
+};
+
+// type UploadPhotos = {
+//     photos: string[];
+// };
+
+const commonHeaders = {
+    "Content-Type": "application/json",
+};
+
+export async function loginAdmin(data: LoginData) {
     try {
         const response = await fetch(`${adminURL}/login`, {
             method: "POST",
             credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
+            headers: commonHeaders,
             body: JSON.stringify(data),
         });
         return response;
     } catch (error) {
         console.log(error);
+        throw error;
     }
 }
 
-export async function createListing(data: any) {
+export async function createListing(data: CreateListing) {
     try {
         const response = await fetch(`${adminURL}/create-listing`, {
             method: "POST",
             credentials: "include",
-            headers: {
-                "Content-Type": "application/json",
-            },
+            headers: commonHeaders,
             body: JSON.stringify(data),
         });
 
         return response.json();
     } catch (error) {
         console.log(error);
+        throw error;
     }
 }
 
@@ -40,5 +60,35 @@ export async function logoutAdmin() {
         });
     } catch (error) {
         console.log(error);
+        throw error;
+    }
+}
+
+export async function uploadPhotoFromLink(link: any) {
+    try {
+        const response = await fetch(`${adminURL}/upload-by-link`, {
+            method: "POST",
+            headers: commonHeaders,
+            body: JSON.stringify({ link }),
+        });
+
+        return response.json();
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export async function uploadPhotoFromDevice(data: any) {
+    try {
+        const response = await fetch(`${adminURL}/upload`, {
+            method: "POST",
+            headers: commonHeaders,
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    } catch (error) {
+        console.log(error);
+        throw error;
     }
 }
